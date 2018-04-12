ff67423c39e78149e2c61b7c2bff9945814171e6
quinn <quinn@atlasagency.com.au>
2018-04-13 09:19:45 +1000

    uploading the next few exercises

--- code_fights.js
+++ code_fights.js
@@ -1,21 +1,64 @@
-// console.log('working');
+// 
+
+// // console.log('working');
 
 
-function centuryFromYear(year) {
+// function centuryFromYear(year) {
 
-    var div = (year/ 100)
-    console.log(div)
+//     var div = (year/ 100)
+//     console.log(div)
     
-    if (year <= 100){
-        console.log(1)
-        // return (1)
-    } else if (div / 1 === 0){
-        console.log(div)
-        // return (div)
-    } else {
-        console.log((Math.floor(div)) + 1)
-        // return ((math.floor(div)) + 1)
+//     if (year <= 100){
+//         console.log(1)
+//         // return (1)
+
+//     } else if (div % 1 != 0){
+//         console.log(div)
+//         // return ((math.floor(div)) + 1)
+//     } else {
+    //         // return (div)
+//     }
+// }
+
+// console.log(centuryFromYear(1900));
+
+
+// function checkPalindrome(inputString) {
+    
+//     console.log(inputString);
+
+//     let og = inputString;
+//     let reverse = inputString.split.string.reverse;
+    
+//     console.log(reverse);
+    
+//     if ( og === reverse ) {
+//         return true
+//     } else {
+//         return false
+//     }
+        
+// }
+
+// checkPalindrome('aabbaa');
+
+
+inputArray = [3, 6, -2, -5, 7, 3];
+
+console.log(inputArray)
+// create array of the multiples
+// compare the numbers in that array
+// 
+
+
+function adjacentElementsProduct(inputArray) {
+    
+    var multiples = [];
+    
+    for (let inputArray = 0; inputArray < inputArray.length; inputArray++) {
+        const element = array[inputArray] * (inputArray + 1);
+        console.log(element);
     }
 }
 
-console.log(centuryFromYear(1900));
+console.log(adjacentElementsProduct());
