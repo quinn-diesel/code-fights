6e7fe974c68c3b6ee984d0e030ae81abab6b4e09
quinn <quinn@atlasagency.com.au>
2018-05-24 16:34:06 +1000

    uploading a the next problem

--- 10.commoncharacterCount.js
+++ 10.commoncharacterCount.js
@@ -9,9 +9,37 @@ console.log('linked');
 
 // Strings have 3 common characters - 2 "a"s and 1 "c".
 
+// PSEUDO
+// break the strings and push into new arrays
+// loop through the first new array
+// find the place of the first letter & splice it from the second new array
+// subtract the second argument length from the filtered array length
+// gives the number of common characters
 
 function commonCharacterCount(s1, s2) {
 
+    let counter = 0;
+
+    let strOne = s1.split("");
+    let strTwo = s2.split("");
+
+    console.log(strOne, strTwo);
+
+
+    for (let i = 0; i < strOne.length; i++) {
+
+        let idx = strTwo.indexOf(strOne[i]);
+
+        if ( idx != -1){
+            // console.log(idx);
+            let removed = strTwo.splice(idx, 1);
+            counter++;
+            console.log(removed);
+
+        }
+        // console.log(counter);
+    }
+    return counter;
 }
 
 
@@ -19,7 +47,7 @@ function commonCharacterCount(s1, s2) {
 console.log(commonCharacterCount( "aabcc", "adcaa"));
 
 // // answer: 4
-// console.log(commonCharacterCount( "zzzz", "zzzzzzz"));
+console.log(commonCharacterCount( "zzzz", "zzzzzzz"));
 
 // // answer: 3
 // console.log(commonCharacterCount("abca", "xyzbac"));
---
+++ 11.isLucky.js
@@ -0,0 +1,88 @@
+// Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
+
+// Given a ticket number n, determine if it's lucky or not.
+
+// Example
+
+// For n = 1230, the output should be
+// isLucky(n) = true;
+// For n = 239017, the output should be
+// isLucky(n) = false.
+
+// ----- ANSWER -----
+
+
+
+
+// ---- TESTS ----- 
+
+
+// Test 1
+// Input:
+// n: 1230
+// Expected Output:
+// true
+
+
+// Test 2
+// Input:
+// n: 239017
+// Expected Output:
+// false
+
+// Test 5
+// Input:
+// n: 134008
+// Expected Output:
+// true
+
+
+// Test 4
+// Input:
+// n: 10
+// Expected Output:
+// false
+
+
+// Test 5
+// Input:
+// n: 11
+// Expected Output:
+// true
+
+
+// Test 6
+// Input:
+// n: 1010
+// Expected Output:
+// true
+
+
+// Test 7
+// Input:
+// n: 261534
+// Expected Output:
+// false
+
+
+// Test 8
+// Input:
+// n: 100000
+// Expected Output:
+// false
+
+
+// Test 9
+// Input:
+// n: 999999
+// Expected Output:
+// true
+
+
+// Test 10
+// Input:
+// n: 123321
+// Expected Output:
+// true
+
+
