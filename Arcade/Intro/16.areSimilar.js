bdb6dcdd3776786e32c0aee07c2685993807fea0
quinn <quinn@atlasagency.com.au>
2018-06-19 10:57:08 +1000

    most recent day upload

--- Arcade/Intro/16.areSimilar.js
+++ Arcade/Intro/16.areSimilar.js
@@ -30,82 +30,150 @@
 
 function areSimilar(a, b) {
 
+    if (a === b) {
+		for (var i = 0; i < a.length; i++) {
+            if (areSimilar(a[i], b[i]) === false) {
+            console.log('false')
+            return false;}
+		}
+	} else {
+		for (var key in a) {
+			if (a.hasOwnProperty(key)) {
+				if (areSimilar(a[key], a[key]) === false) return false;
+			}
+		}
+	}
+
 }
+
+// ------  LONG ANSWER -------
+
+// var areSimilar = function (value, other) {
+
+// 	// Get the value type
+// 	var type = Object.prototype.toString.call(value);
+
+// 	// If the two objects are not the same type, return false
+// 	if (type !== Object.prototype.toString.call(other)) return false;
+
+// 	// If items are not an object or array, return false
+// 	if (['[object Array]', '[object Object]'].indexOf(type) < 0) return false;
+
+// 	// Compare the length of the length of the two items
+// 	var valueLen = type === '[object Array]' ? value.length : Object.keys(value).length;
+// 	var otherLen = type === '[object Array]' ? other.length : Object.keys(other).length;
+// 	if (valueLen !== otherLen) return false;
+
+// 	// Compare two items
+// 	var compare = function (item1, item2) {
+
+// 		// Get the object type
+// 		var itemType = Object.prototype.toString.call(item1);
+
+// 		// If an object or array, compare recursively
+// 		if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
+// 			if (!isEqual(item1, item2)) return false;
+// 		}
+
+// 		// Otherwise, do a simple comparison
+// 		else {
+
+// 			// If the two items are not the same type, return false
+// 			if (itemType !== Object.prototype.toString.call(item2)) return false;
+
+// 			// Else if it's a function, convert to a string and compare
+// 			// Otherwise, just compare
+// 			if (itemType === '[object Function]') {
+// 				if (item1.toString() !== item2.toString()) return false;
+// 			} else {
+// 				if (item1 !== item2) return false;
+// 			}
+
+// 		}
+// 	};
+
+// 	// Compare properties
+// 	if (type === '[object Array]') {
+// 		for (var i = 0; i < valueLen; i++) {
+// 			if (compare(value[i], other[i]) === false) return false;
+// 		}
+// 	} else {
+// 		for (var key in value) {
+// 			if (value.hasOwnProperty(key)) {
+// 				if (compare(value[key], other[key]) === false) return false;
+// 			}
+// 		}
+// 	}
+
+// 	// If nothing failed, return true
+// 	return true;
+
+// };
 
 
 // Test 1
 // Input:
-// a: [1, 2, 3]
-// b: [1, 2, 3]
+// areSimilar([1, 2, 3], [1, 2, 3])
 // Expected Output:
 // true
 
 
 // Test 2
 // Input:
-// a: [1, 2, 3]
-// b: [2, 1, 3]
+areSimilar([1, 2, 3], [2, 1, 3])
 // Expected Output:
 // true
 
 
 // Test 3
 // Input:
-// a: [1, 2, 2]
-// b: [2, 1, 1]
+// areSimilar([1, 2, 2], [2, 1, 1])
 // Expected Output:
 // false
 
 
 // Test 4
 // Input:
-// a: [1, 1, 4]
-// b: [1, 2, 3]
+// areSimilar([1, 1, 4], [1, 2, 3])
 // Expected Output:
 // false
 
 
 // Test 5
 // Input:
-// a: [1, 2, 3]
-// b: [1, 10, 2]
+// areSimilar([1, 2, 3], [1, 10, 2])
 // Expected Output:
 // false
 
 
 // Test 6
 // Input:
-// a: [2, 3, 1]
-// b: [1, 3, 2]
+// areSimilar([2, 3, 1], [1, 3, 2])
 // Expected Output:
 // true
 
 //Test 7
 // Input:
-// a: [2, 3, 9]
-// b: [10, 3, 2]
+// areSimilar([2, 3, 9], [10, 3, 2])
 // Expected Output:
 // false
 
 
 // Test 8
 // Input:
-// a: [4, 6, 3]
-// b: [3, 4, 6]
+// areSimilar([4, 6, 3], [3, 4, 6])
 // Expected Output:
 // false
 
 
 // Test 9
 // Input:
-// a: [832, 998, 148, 570, 533, 561, 894, 147, 455, 279]
-// b: [832, 998, 148, 570, 533, 561, 455, 147, 894, 279]
+// areSimilar([832, 998, 148, 570, 533, 561, 894, 147, 455, 279], [832, 998, 148, 570, 533, 561, 455, 147, 894, 279])
 // Expected Output:
 // true
 
 // Test 10
 // Input:
-// a: [832, 998, 148, 570, 533, 561, 894, 147, 455, 279]
-// b: [832, 570, 148, 998, 533, 561, 455, 147, 894, 279]
+// areSimilar([832, 998, 148, 570, 533, 561, 894, 147, 455, 279], [832, 570, 148, 998, 533, 561, 455, 147, 894, 279])
 // Expected Output:
 // false
